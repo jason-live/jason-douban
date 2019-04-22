87aede41668d4f92fba6771456356135fb71db6e
袁杨 <yuanyang@uvoplus.com>
2019-04-22 23:51:32 +0800

    daily-2-2

---
+++ test/co.js
@@ -0,0 +1,38 @@
+const co = require('co');
+const fetch = require('node-fetch');
+
+/** 
+ * co
+*/
+// co(function  *() {
+//   const res = yield fetch('https://api.douban.com/v2/movie/1291843');
+//   const movie = yield res.json();
+//   const summary = movie.summary;
+//   console.log(summary);
+// });
+
+/** 
+ * 手写co
+*/
+
+// function run (generator) {
+//   const iterator = generator();
+//   const it = iterator.next();
+//   const promise = it.value;
+
+//   promise.then(data => {
+//     const it2 = iterator.next(data);
+//     const promise2 = it2.value;
+
+//     promise2.then(data2 => {
+//       iterator.next(data2);
+//     });
+//   });
+// };
+
+// return(function  *() {
+//   const res = yield fetch('https://api.douban.com/v2/movie/1291843');
+//   const movie = yield res.json();
+//   const summary = movie.summary;
+//   console.log(summary);
+// });
