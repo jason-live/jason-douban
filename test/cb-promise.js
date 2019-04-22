e8cf8e843cd6c5774aa83857dab4d5ece4b5403c
袁杨 <yuanyang@uvoplus.com>
2019-04-22 22:58:41 +0800

    daily-1-3

---
+++ test/cb-promise.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+/**
+ * callback
+ */
+// fs.readFile('./package.json', (error, data) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   };
+//   const json = JSON.parse(data);
+//   console.log(json.name);
+// });
+
+/**
+ * Promise
+ */
+// function readFileAsync(path) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile('./package.json', (error, data) => {
+//       if (error) {
+//         reject(error);
+//         return;
+//       };
+//       resolve(data);
+//     });
+//   });
+// };
+
+// readFileAsync('./package.json')
+//   .then((data) => {
+//     const json = JSON.parse(data);
+//     console.log(json.name);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+  /** 
+   * promisify
+   */
+
+const util = require('util');
+
+util.promisify(fs.readFile)('./package.json')
+  .then(JSON.parse)
+  .then((data) => {
+    console.log(data.name);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
